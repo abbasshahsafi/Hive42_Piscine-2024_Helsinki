bbbb3ca937e2797a2beadc640af60e3cf80b0549
Abbas Shahsafi <ashahsaf@c3r4p6.hive.fi>
2024-07-16 14:09:03 +0300

    add main to C05

---
+++ C05/With Main File/ex01/ft_recursive_factorial.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ashahsaf <ashahsaf@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/14 12:18:28 by ashahsaf          #+#    #+#             */
+/*   Updated: 2024/07/14 12:19:42 by ashahsaf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	if (nb == 0 || nb == 1)
+		return (1);
+	return (nb * ft_recursive_factorial(nb - 1));
+}
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%i", ft_recursive_factorial(9));
+}
---
+++ C05/With Main File/ex02/ft_iterative_power.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ashahsaf <ashahsaf@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/14 12:21:49 by ashahsaf          #+#    #+#             */
+/*   Updated: 2024/07/14 12:22:42 by ashahsaf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	i;
+
+	i = nb;
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	while (power > 1)
+	{
+		nb *= i;
+		power--;
+	}
+	return (nb);
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%i", ft_iterative_power(2, 2 ));
+}
