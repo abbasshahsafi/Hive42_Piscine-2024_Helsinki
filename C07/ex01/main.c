387f1e364785638ea60e6c62cd73c3916f5ec152
Abbas Shahsafi <ashahsaf@c3r4p6.hive.fi>
2024-07-16 14:07:13 +0300

    add main file to C07

---
+++ C07/ex01/main.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ashahsaf <ashahsaf@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 14:06:10 by ashahsaf          #+#    #+#             */
+/*   Updated: 2024/07/16 14:06:16 by ashahsaf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int	min;
+	int	max;
+	int	*tab;
+	int	i = 0;
+	int	size;
+
+	min = 111;
+	max = 125;
+	size = max - min;
+	tab = ft_range(min, max);
+	while(i < size)
+	{
+		printf("%d, ", tab[i]);
+		i++;
+	}
+}
